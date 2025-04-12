01ed621a56992f0dddfba5be696fb8d552d83b38
etfroeland <etfroeland@gmail.com>
2025-04-12 11:44:42 +0200

    Debugging

--- interfaces/ros1/uw_depth_ros/src/uw_depth_node.py
+++ interfaces/ros1/uw_depth_ros/src/uw_depth_node.py
@@ -11,6 +11,7 @@ from cv_bridge import CvBridge
 from torchvision.transforms import ToTensor
 import sys
 import os
+from PIL import Image as PILImage
 
 # Get the parent directory four levels up
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', '..'))
@@ -25,33 +26,30 @@ from depth_estimation.utils.depth_prior import get_depth_prior_from_features
 # Load model once and keep in memory
 MODEL_PATH = os.path.join(parent_dir, "data", "saved_models", "model_e11_udfnet_lr0.0001_bs6_lrd0.9_with_infguidance.pth")
 
+
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 class UWDepthNode:
     def __init__(self):
         rospy.init_node("uw_depth_node", anonymous=True)
 
+        self.use_priors = rospy.get_param("~use_priors", True)
+
         # OpenCV Bridge
         self.bridge = CvBridge()
 
-        # # Subscribers (Using message_filters for synchronization)
-        # self.image_sub = Subscriber("/camera/image_raw", Image)
-        # self.prior_sub = Subscriber("/depth_estimation/prior", PointCloud)
+        # Subscribers (Using message_filters for synchronization)
+        self.image_sub = Subscriber("/ted/image", Image)
+        self.prior_sub = Subscriber("/fft/depth_pointcloud", PointCloud)
 
-        # # Synchronize messages
-        # self.sync = ApproximateTimeSynchronizer([self.image_sub, self.prior_sub], queue_size=10, slop=0.1)
-        # self.sync.registerCallback(self.callback)
-
-        #For testing
-        self.prior_sub = Subscriber("/depth_estimation/prior", PointCloud)
         # Synchronize messages
-        self.sync = ApproximateTimeSynchronizer([self.prior_sub], queue_size=10, slop=0.1)
+        self.sync = ApproximateTimeSynchronizer([self.image_sub, self.prior_sub], queue_size=10, slop=0.1)
         self.sync.registerCallback(self.callback)
-
-
 
         # Publisher
         self.depth_pub = rospy.Publisher("/uw_depth/depth_image", Image, queue_size=1)
+        self.heatmap_pub = rospy.Publisher("/uw_depth/heatmap", Image, queue_size=1)
+        self.combined_pub = rospy.Publisher("/uw_depth/combined_image", Image, queue_size=1)
 
         # Load Model
         rospy.loginfo(f"Loading model on {DEVICE}")
@@ -60,55 +58,72 @@ class UWDepthNode:
         self.model.eval()
         rospy.loginfo("Model loaded successfully.")
 
-    def callback(self, prior_msg):
+    def callback(self, image_msg, prior_msg):
+        rospy.loginfo("Received image and point cloud messages.")
         try:
-            # # Convert ROS image to OpenCV format
-            # cv_rgb = self.bridge.imgmsg_to_cv2(rgb_msg, desired_encoding="bgr8")
+            total_time = time.time()
 
-            #For testing
-            image_path = os.path.join(parent_dir, "interfaces", "ros1", "uw_depth_ros", "src", "image.jpg")
-            cv_rgb = cv2.imread(image_path)
+            # Convert ROS image to OpenCV format
+            cv_rgb = self.bridge.imgmsg_to_cv2(image_msg, desired_encoding="bgr8")
 
             # Convert ROS pointcloud to numpy array
             points = prior_msg.points
-            priors = np.array([[point.x, point.y, point.z] for point in points])
-            # Convert to tensor
-            features = torch.tensor(priors, dtype=torch.float32).unsqueeze(0)
+            priors = np.array([[point.x, point.y, point.z] for point in points], dtype=np.float32)
 
-            # Convert to tensor and resize
-            orig_rgb_tensor = ToTensor()(cv_rgb).unsqueeze(0).to(DEVICE)
-            resized_rgb_tensor = torch.nn.functional.interpolate(orig_rgb_tensor, (480, 640), mode="bilinear", align_corners=False)
-            prior_tensor = get_depth_prior_from_features(features).to(DEVICE)
+            tensor_conversion_time = time.time()
+
+            # Convert prior to tensor
+            features = torch.tensor(priors, dtype=torch.float32).unsqueeze(0)
+            prior_tensor = get_depth_prior_from_features(features).to(DEVICE)            # get_depth_prior_from_features is the bottleneck
+            
+            # Convert image to tensor
+            resized_cv_rgb = cv2.resize(cv_rgb, (640, 480), interpolation=cv2.INTER_LINEAR)
+            rgb_tensor = ToTensor()(resized_cv_rgb).unsqueeze(0).to(DEVICE)
+            print("Tensor conversion time: ", time.time() - tensor_conversion_time)
 
             # Run inference
             start_time = time.time()
             with torch.no_grad():
-                prediction, _ = self.model(resized_rgb_tensor, prior_tensor)
+                prediction, _ = self.model(rgb_tensor, prior_tensor)
             end_time = time.time()
-            print("Prediction shape: ", prediction.shape)
-
             rospy.loginfo(f"Inference Time: {(end_time - start_time):.4f} sec")
-
-            # # Remove batch dimension
-            # heatmap = prediction[0].squeeze(0).cpu().numpy()
-
-            # # Convert the heatmap to a color image (hot color map)
-            # colored_heatmap = cv2.applyColorMap((heatmap * 255).astype(np.uint8), cv2.COLORMAP_HOT)
-
-            # # Convert to a ROS image message (appropriate encoding for color images)
-            # depth_msg = self.bridge.cv2_to_imgmsg(colored_heatmap, encoding="bgr8")
-
-            # Get the raw depth prediction (depth in centimeters)
-            depth_map_cm = prediction[0].squeeze(0).cpu().numpy()  # Depth map as a 2D numpy array in centimeters
-
-            # Option 1: Convert depth to millimeters and use 16-bit unsigned integer
-            depth_map_mm = (depth_map_cm * 10).astype(np.uint16)  # Convert cm to mm
-
-            # Convert to ROS message (using 16-bit unsigned integer encoding for millimeters)
-            depth_msg = self.bridge.cv2_to_imgmsg(depth_map_mm, encoding="16UC1")
-
-            # Publish result
+        
+            # Depth map as a 2D numpy array in meters
+            depth_map = prediction[0].squeeze(0).cpu().numpy()
+            depth_msg = self.bridge.cv2_to_imgmsg((depth_map).astype(np.float32), encoding="32FC1")  # Use 32-bit float encoding for depth images
+            print(depth_map.mean())
+            depth_msg.header = image_msg.header  # Set the header to match the input image
+            depth_msg.header.frame_id = "camera_frame"
             self.depth_pub.publish(depth_msg)
+
+            # For visualization ------------------------------------------------------------------------------------------------------------------------------
+            depth_map_scaled = np.minimum(depth_map, 3.0) / 3.0  # Scale to [0, 1], values above 3m are saturated
+            depth_gray = (depth_map_scaled * 255).astype(np.uint8)  # Convert to 8-bit grayscale
+
+            # Apply inferno colormap for consistent visualization
+            colored_heatmap = cv2.applyColorMap(depth_gray, cv2.COLORMAP_INFERNO)
+            heatmap_msg = self.bridge.cv2_to_imgmsg(colored_heatmap, encoding="bgr8")
+
+            # Resize RGB to match heatmap size
+            resized_cv_rgb = cv2.resize(resized_cv_rgb, (colored_heatmap.shape[1], colored_heatmap.shape[0]), interpolation=cv2.INTER_LINEAR)
+
+            # Combine RGB and heatmap horizontally
+            combined_image = np.hstack((resized_cv_rgb, colored_heatmap))
+            combined_image_msg = self.bridge.cv2_to_imgmsg(combined_image, encoding="bgr8")
+
+
+            # Publish vizualisation result
+            self.heatmap_pub.publish(heatmap_msg)
+            self.combined_pub.publish(combined_image_msg)
+            # End of visualization ---------------------------------------------------------------------------------------------------------------------------
+            print("Total_time: ", time.time() - total_time)
+
+            # Save the priors as a csv file with filename as image seq
+            # seq = image_msg.header.seq
+            # prior_file = os.path.join(parent_dir, "depth_priors", f"{seq:04d}_prior.csv")
+            # np.savetxt(prior_file, priors, delimiter=",", header="x,y,z", comments='')
+
+            
 
         except Exception as e:
             rospy.logerr(f"Error processing image: {e}")
